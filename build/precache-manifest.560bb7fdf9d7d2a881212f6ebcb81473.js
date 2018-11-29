2c4945f8f8436bbd1af743adf43d9f847181e3e0
martinwithyou <406410589@qq.com>
2018-11-29 16:17:17 +0800

    bugf ixed

---
+++ build/precache-manifest.560bb7fdf9d7d2a881212f6ebcb81473.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "70fe4f3106e1ad4464e6",
+    "url": "/static/js/main.70fe4f31.chunk.js"
+  },
+  {
+    "revision": "99f4516149651e5e1e5e",
+    "url": "/static/js/1.99f45161.chunk.js"
+  },
+  {
+    "revision": "70fe4f3106e1ad4464e6",
+    "url": "/static/css/main.a00f7520.chunk.css"
+  },
+  {
+    "revision": "5b959940c0f968363b5e86d59b095e42",
+    "url": "/index.html"
+  }
+];
